6b10f0b4a4f1f6bb4cfa40ab7d22dabb7c4c7406
Rupu <rupuislamashraful7@gmail.com>
2020-02-10 22:05:37 +0600

    Initial Commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by be RUPU on 30/1/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    
+    let last : Double
+    
+}
+
+
